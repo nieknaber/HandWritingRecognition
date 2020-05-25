8a2a7ffacb1567abe0d3c7e2307d3d65fd39d047
Niek Naber <niek_naber@hotmail.com>
2020-05-25 15:07:11 +0200

    some small docs in the main file

--- process.py
+++ process.py
@@ -3,10 +3,9 @@ from LineSegmentation.lineSegment import *
 
 
 for i in range(1,9):
-    (theta, linePoints, grayImg) = lineSegment("images/0" + str(i) + ".jpg") 
 
-# just to show all segments:
-# for segment in segments:
-#     cv.namedWindow("Window", flags=cv.WINDOW_NORMAL)
-#     cv.imshow("Window", segment)
-#     k = cv.waitKey(0)
+    (theta, linePoints, grayImg) = lineSegment("images/0" + str(i) + ".jpg") 
+    # theta can be stored so that we can use it later as a feature
+    # linePoints contain pairs of two (x,y) points. 
+    # A* algorithm can be used to separate lines by using the first point as a start, second as end.
+    # grayImg is the binary gray image where 0 = white, 255 = black
