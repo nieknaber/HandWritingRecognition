00823b982cb3317580e7b935a344a887fdac823a
Maximilian Velich <max@MaxBook.local>
2020-05-27 20:36:41 +0200

    Downloaded all pictures of Herodian and imported them all into the main.py file. However, for some reason each folder contains two versions for each character.

--- Character Segmentation/main.py
+++ Character Segmentation/main.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+from PIL import Image
 from helper import *
 from CharacterSegmentation import *
 
@@ -15,5 +16,20 @@ featuresPerSegment = 8
 featuresPerWindow = featuresPerSegment * (2*12)
 
 # Examine training data
-arr = os.listdir()
-print(arr)
+
+# i = Image.open('./monkbrill/Resh/navis-QIrug-Qumran_extr09_2357-line-007-y1=392-y2=514-zone-HUMAN-x=0711-y=0020-w=0032-h=0067-ybas=0040-nink=443-segm=COCOS5cocos.pgm')
+# i.show()
+
+characterLocation = "Herodian"
+characters = os.listdir(characterLocation)
+for character in characters:
+    if not character.startswith('.'):
+        files = os.listdir(characterLocation + "/" + character)
+        for f in files:
+            if not f.startswith('.'):
+                imagePath = characterLocation + '/' + character + '/' + f
+                # print(imagePath)
+                image = Image.open(imagePath)
+                (height, width) = np.shape(image)
+                print(height, width)
+
