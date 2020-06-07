3db1526e3a32831629d9b388866b1c1da5606abf
Niek Naber <niek_naber@hotmail.com>
2020-06-07 22:29:28 +0200

    added deslanting

--- process.py
+++ process.py
@@ -1,7 +1,11 @@
 import sys
 import time
+from SlantNormalization.slantNormalize import *
 from LineSegmentation.lineSegment import *
 from LineSegmentation.lineSegmentAStar import *
+from SlantNormalization.slantNormalize import *
+
+SAVEPICTURES = True # set to false if you do not want to save pictures in between steps
 
 img = getImage("images/20.jpg")
 rotatedImage, slope = findSlope(img, 10, 1)
@@ -11,6 +15,15 @@ images = lineSegmentAStar(rotatedImage)
 
 for i in range(0,len(images)):
     images[i] = np.transpose(images[i])
-    cv.imwrite("line " + str(i) + ".bmp", images[i] * 255)
+    if SAVEPICTURES: cv.imwrite("line_" + str(i) + ".bmp", (1 - images[i]) * 255)
 
-# images is now the list of slope corrected line segments
+slantAngles = []
+for i in range(0,len(images)):
+    slantAngle, dst = slantNormalize(images[i])
+    if SAVEPICTURES: cv.imwrite("line_" + str(i) + "_deslanted.bmp", (1 - dst) * 255)
+    images[i] = dst
+    slantAngles.append(slantAngle)
+
+# images[] is now the list of slope corrected line segments
+# slantAngles[] is the list of slants per line segment (same indexes)
+# slope is the line slope angle
