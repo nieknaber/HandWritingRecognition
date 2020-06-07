e1399dedc1a104a22dbc426fb04f289b5b68e160
Niek Naber <niek_naber@hotmail.com>
2020-06-07 20:48:27 +0200

    small clean up. images is now clear array of images which can be used next

--- process.py
+++ process.py
@@ -4,29 +4,13 @@ from LineSegmentation.lineSegment import *
 from LineSegmentation.lineSegmentAStar import *
 
 img = getImage("images/20.jpg")
-rotatedImage,slope = findSlope(img, 10, 1)
-print("Best angle: ",slope)
-
-#cv.namedWindow("Window", flags=cv.WINDOW_NORMAL)
-#cv.imshow("Window",rotatedImage)
-#k = cv.waitKey(0)
+rotatedImage, slope = findSlope(img, 10, 1)
+print("Best angle: ", slope)
 
 images = lineSegmentAStar(rotatedImage)
 
 for i in range(0,len(images)):
-    cv.imwrite("line " + str(i) + ".bmp", np.transpose(images[i] * 255))
+    images[i] = np.transpose(images[i])
+    cv.imwrite("line " + str(i) + ".bmp", images[i] * 255)
 
-
-    
-
-# for i in range(1,9):
-#
-#     (theta, linePoints, grayImg) = lineSegment("images/0" + str(i) + ".jpg")
-#     # theta can be stored so that we can use it later as a feature
-#     # linePoints contain pairs of two (x,y) points.
-#     # A* algorithm can be used to separate lines by using the first point as a start, second as end.
-#     # grayImg is the binary gray image where 0 = white, 255 = black
-#
-#     # slope detection:
-#     print("Computing image ",i)
-#     projections("images/0" + str(i) + ".jpg")
+# images is now the list of slope corrected line segments
