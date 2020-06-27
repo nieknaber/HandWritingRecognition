eceacc8951cd478a5c34ee56c652939b8736d3ef
Maximilian Velich <max@MaxBook.local>
2020-06-27 15:05:01 +0200

    Implemented saving mechanism for character classification

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from src.network_controller import Network_Controller
 
 from src.data_preparation import helper as h
 
+import json
+import numpy as np
+
 ### Global Variable ################################################
 
 segment_dim = 30
@@ -28,17 +31,36 @@ def test_Network_Training():
 
 def test_Character_Classfication():
 
+    ### INPUT
+    dummy = h.getImage("./src/resources/test_data_lines/line_0.bmp")
+
     num_inputs = num_directions * window_dim[0] * window_dim[1]
     segment_size = (segment_dim, segment_dim)
     window_size = (segment_dim*window_dim[0], segment_dim*window_dim[1])
-    dummy = h.getImage("./src/resources/test_data_lines/line_0.bmp")
     model_path = './src/cached_data/trained_models/model_200_144.pt'
 
     cc = Character_Classification_Controller(segment_size, window_size, model_path, num_inputs)
     result = cc.run_classification(dummy)
 
+    ### OUTPUT
     (windows, labels) = result
 
-    print(windows[0])
-    print(labels)
+    ### SAVING
+    json.dump([window.tolist() for window in windows], open('./src/cached_data/classified_characters/test_windows.json', 'w'))
+    json.dump(labels, open('./src/cached_data/classified_characters/test_labels.json', 'w'))
+
+test_Character_Classfication()
     
+def test_style_classification():
+
+    ### INPUT
+
+
+    newLabels = [label.capitalize() for label in labels]
+    newImgs = [img.astype(np.uint8) for img in imgs]
+    print("doint style classification")
+    styleClassifier = Classifier("char_num_acc_lda_k3.txt", 3)
+    style = styleClassifier.classifyList(newImgs, newLabels)
+    print(style)
+    print(labels[1])
+
